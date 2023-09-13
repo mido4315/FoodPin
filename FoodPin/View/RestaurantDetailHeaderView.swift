8914bf38fcd3bd500e20ed26e2134d99a5b24a11
mohamed <mohmedmahmoud3@gmail.com>
2023-09-13 05:51:58 +0300

    create RestaurantDetailHeaderView in IB

---
+++ FoodPin/View/RestaurantDetailHeaderView.swift
@@ -0,0 +1,20 @@
+//
+//  RestaurantDetailHeaderView.swift
+//  FoodPin
+//
+//  Created by mohamed mahmoud on 13/09/2023.
+//
+
+import UIKit
+
+class RestaurantDetailHeaderView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
